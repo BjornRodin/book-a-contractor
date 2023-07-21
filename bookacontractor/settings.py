eb2e708ae47f9d2b161c0455cb6bf4f7ceeb6c82
BjornRodin <bjornrodin@gmail.com>
2023-07-21 13:59:55 +0000

    Changed ALLOWED_HOSTS due to error

--- bookacontractor/settings.py
+++ bookacontractor/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['book-a-contractor.herokuapp.com', '8000-bjornrodin-bookacontrac-wjkm9mrzy7o.ws-eu102.gitpod.io']
+ALLOWED_HOSTS = ['book-a-contractor-e3bf2a381fca.herokuapp.com', '8000-bjornrodin-bookacontrac-wjkm9mrzy7o.ws-eu102.gitpod.io']
 
 
 # Application definition
