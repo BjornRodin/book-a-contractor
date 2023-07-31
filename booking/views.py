61224674459dc7626ae8fb71839dd97bdfffe5ec
BjornRodin <bjornrodin@gmail.com>
2023-07-31 13:34:35 +0000

    Add redirect and login_required to views.py

--- booking/views.py
+++ booking/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     return render(request, 'index.html')
 
+@login_required
 def my_bookings(request):
     return render(request, 'my_bookings.html')
 
@@ -11,3 +13,7 @@ def register(request):
 
 def login_user(request):
     return render(request, 'login.html')
+
+@login_required
+def booking_form(request):
+    return render(request, 'booking_form.html')
