31d39e4ff7fe93fbe8010304122372ae13b3da1d
BjornRodin <bjornrodin@gmail.com>
2023-07-31 12:47:26 +0000

    Connected views and urls with templates for each page on the website

--- booking/urls.py
+++ booking/urls.py
@@ -3,4 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('my-bookings/', views.my_bookings, name='my-bookings'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login_user, name='login'),
 ]
--- booking/views.py
+++ booking/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def my_bookings(request):
+    return render(request, 'my_bookings.html')
+
+def register(request):
+    return render(request, 'register.html')
+
+def login_user(request):
+    return render(request, 'login.html')
