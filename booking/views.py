df7a04a44718edc3fb1ea0bbe6dcc9c7bc7385fa
BjornRodin <bjornrodin@gmail.com>
2023-07-31 20:11:47 +0000

    Add login form to login.html and a custom view in views.py

--- booking/views.py
+++ booking/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
 
 def index(request):
     return render(request, 'index.html')
@@ -17,3 +19,7 @@ def login_user(request):
 @login_required
 def booking_form(request):
     return render(request, 'booking_form.html')
+
+class CustomLoginView(LoginView):
+    template_name = 'login.html'
+    success_url = reverse_lazy('booking-form')
