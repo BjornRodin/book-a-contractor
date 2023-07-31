e1c2de31bf3fdc927faf8080a2b1ca9ed9cb4541
BjornRodin <bjornrodin@gmail.com>
2023-07-31 19:51:40 +0000

    Corrected a syntax error in base.html and bookings urls.py

--- booking/urls.py
+++ booking/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('my-bookings/', views.my_bookings, name='my-bookings'),
     path('register/', views.register, name='register'),
     path('login/', views.login_user, name='login'),
-    path('booking-form/', views.booking_form, name='booking-form')
+    path('booking-form/', views.booking_form, name='booking-form'),
 ]
