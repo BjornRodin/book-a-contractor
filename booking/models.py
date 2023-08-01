25e9510f7bab67cd349e1ed80f04c4f501005003
BjornRodin <bjornrodin@gmail.com>
2023-08-01 16:34:59 +0000

    Add Booking model to model.py to handle bookings in the database

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+# Model for booking a session
+class Booking(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    date_and_time = models.DateTimeField()
+    project_type = models.CharField(max_length=50)
+    project_details = models.TextField(max_length=250)
