c68b3aa24786fe3007d0a4b7bba335b22158eaa2
BjornRodin <bjornrodin@gmail.com>
2023-08-03 10:35:51 +0000

    Connect update-booking in urls.py

--- booking/urls.py
+++ booking/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('booking-form/', views.booking_form, name='booking-form'),
     path('view-booking/<int:pk>/', views.view_booking, name='view-booking'),
     path('delete-booking/<int:pk>/', views.delete_booking, name='delete-booking'),
+    path('update-booking/<int:pk>/', views.update_booking, name='update-booking'),
 ]
