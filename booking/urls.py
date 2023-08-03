3c4ee6f5821396ce285afa3f98e8dd8e51dcdc90
BjornRodin <bjornrodin@gmail.com>
2023-08-03 18:00:46 +0000

    Reset Debug to True and updated urls.py

--- booking/urls.py
+++ booking/urls.py
@@ -10,8 +10,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
     path('booking-form/', views.booking_form, name='booking-form'),
-    path('booking-form/<int:pk>/', views.booking_form, name='booking-form'),
+    path('update-booking/<int:pk>/', views.booking_form, name='update-booking'),
     path('view-booking/<int:pk>/', views.view_booking, name='view-booking'),
     path('delete-booking/<int:pk>/', views.delete_booking, name='delete-booking'),
-    path('update-booking/<int:pk>/', views.update_booking, name='update-booking'),
 ]
