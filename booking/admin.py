aa149f8fc33bef64077c344266c584a2d3c59c74
BjornRodin <bjornrodin@gmail.com>
2023-08-04 08:46:29 +0000

    Add 'BookingAdmin' to admin.py to customise adminpanel

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from .models import Booking
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+@admin.register(Booking)
+class BookingAdmin(SummernoteModelAdmin):
+    """
+    Adds 'Bookings' to the admin panel where an admin can view all bookings
+    Features added to view certain information in a list,
+    Search for specific elements
+    A filter to quickly check bookings within that filter
+    And fields for textareas so they are more customizable and clearly visible when viewing a booking
+    """
+    list_display = (
+        'first_name',
+        'last_name',
+        'email',
+        'date',
+        'time',
+        'project_type'
+    )
+    search_fields = (
+        'first_name',
+        'last_name',
+        'email',
+        'project_type'
+    )
+    list_filter = (
+        'first_name',
+        'last_name',
+        'email',
+        'date',
+        'time',
+        'project_type'
+    )
+    summernote_fields = (
+        'project_type',
+        'project_details'
+    )
